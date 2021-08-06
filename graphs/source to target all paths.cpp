9a39c1a0d0fa9196808143773d72a6ae5409b5aa
unknown <jashwanthbj9@gmail.com>
2021-08-07 01:26:31 +0530

    adding files

--- graphs/source to target all paths.cpp
+++ graphs/source to target all paths.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 vector<vector<int>>allPaths;
 int target = 3;
-void dfs(int src, vector<int>&vis, vector<int>adj[],vector<int>V)
+void dfs(int src, vector<int>&vis, vector<int>adj[], vector<int>V)
 {
 	vis[src] = 1;
 	V.push_back(src);
@@ -12,12 +12,12 @@ void dfs(int src, vector<int>&vis, vector<int>adj[],vector<int>V)
 		allPaths.push_back(V);
 	}
 	for (auto x : adj[src])
-		if (!vis[x]) dfs(x, vis, adj,V);
+		if (!vis[x]) dfs(x, vis, adj, V);
 	vis[src] = 0;
 }
 int main()
 {
-  // 0->1 1->2 0->2  2->3;
+	// 0->1 1->2 0->2  2->3;
 	int n, v;
 	cin >> n >> v;
 	vector<int>adj[n];
@@ -30,7 +30,7 @@ int main()
 	}
 	vector<int>vis(v, 0);
 	vector<int>V;
-	dfs(0, vis, adj,V);
+	dfs(0, vis, adj, V);
 	for (auto x : allPaths)
 	{
 		for (auto y : x)
