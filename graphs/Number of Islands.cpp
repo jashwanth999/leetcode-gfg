c15ad59f968812c3135edc78c3ade7660ca29dba
unknown <jashwanthbj9@gmail.com>
2021-08-06 10:43:57 +0530

    adding files

--- graphs/Number of Islands.cpp
+++ graphs/Number of Islands.cpp
@@ -1,35 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
-void dfs(vector<vector<char>>&grid, int i, int j)
+void dfs(vector<vector<char>>&grid, int i, int j, int &c)
 {
 	if (i<0 or j<0 or i >= grid.size() or j >= grid[0].size() or grid[i][j] == '0' or grid[i][j] == 'V') return;
 	grid[i][j] = 'V';
-	dfs(grid, i + 1, j);
-	dfs(grid, i - 1, j);
-	dfs(grid, i, j - 1);
-	dfs(grid, i, j + 1);
+	c++;
+	dfs(grid, i + 1, j, c);
+	dfs(grid, i - 1, j, c);
+	dfs(grid, i, j - 1, c);
+	dfs(grid, i, j + 1, c);
+	dfs(grid, i + 1, j + 1, c);
+	dfs(grid, i - 1, j + 1, c);
+	dfs(grid, i - 1, j - 1, c);
+	dfs(grid, i + 1, j - 1, c);
 
 }
 int main()
 {
 	vector<vector<char>>grid =
-	{	
-	{'0', '1', '1', '1', '0', '0', '0'},
-	 {'0', '0', '1', '1', '0', '1', '0'}
+	{
+		{ '0', '0', '1', '1', '0' },
+		{ '1', '0', '1', '1', '0' },
+		{ '0', '1', '0', '0', '0' },
+		{ '0', '0', '0', '0', '1' }
 	};
 	int numberOfIslands = 0;
+	int maxi = 0;
 	for (int i = 0; i < grid.size(); i++) {
 		for (int j = 0; j < grid[0].size(); j++)
 		{
 			if (grid[i][j] == '1')
 			{
 				numberOfIslands++;
-				dfs(grid, i, j);
+				int c = 0;
+				dfs(grid, i, j, c);
+				maxi = max(c, maxi);
 			}
 		}
 	}
-	cout << numberOfIslands << endl;
+	cout << maxi << endl;
 
 
 
